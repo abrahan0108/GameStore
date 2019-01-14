b4a6b0d487691378c0b4da3fa406d6a75e5808f0
Abrahan <f8abrahan@gmail.com>
2019-01-14 15:49:02 -0600

    Se agrega el signup

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+from .forms import AuthenticationForm
 
 
 urlpatterns = [
+    path(r'accounts/login/', login, {
+        'template_name': 'login.html',
+        'authentication_form': AuthenticationForm
+    }, name='login'),
+    path(r'accounts/logout/', logout, {
+        'next_page': '/'
+    }, name='logout'),
     path(r'', views.index, name='index'),
+    path(r'accounts/signup/', views.signup, name='signup'),
 ]
